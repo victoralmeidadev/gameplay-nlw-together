8e8e0b5244942b2d4819faac445ecfbb6a58b984
Victor Almeida <victooralmeida1@gmail.com>
2021-06-22 00:22:58 -0300

    refactor: refactor ButtonIcon component to use RectButton

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,26 +1,20 @@
 import React from "react";
-
-import {
-  Text,
-  Image,
-  View,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from "react-native";
+import { Text, Image, View } from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 
 import DiscordImg from "../../assets/discord.png";
 import { styles } from "./styles";
 
-type Props = TouchableOpacityProps & {
+type Props = RectButtonProps & {
   title: string;
 };
 export function ButtonIcon({ title, ...props }: Props) {
   return (
-    <TouchableOpacity style={styles.container} {...props}>
+    <RectButton style={styles.container} {...props}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon} />
       </View>
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
