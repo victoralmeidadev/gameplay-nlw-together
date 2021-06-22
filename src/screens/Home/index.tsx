fa4f6b4b02ca47e02a158b21895048215241af11
Victor Almeida <victooralmeida1@gmail.com>
2021-06-22 00:26:47 -0300

    feat: add components to Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,59 @@
+import React, { useCallback, useState } from "react";
+import { View, FlatList, Text } from "react-native";
+
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { Profile } from "../../components/Profile";
+import { CategorySelect } from "../../components/CategorySelect";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
+
+import { styles } from "./styles";
+
+export function Home() {
+  const [category, setCategory] = useState("");
+
+  const appointments = [
+    {
+      id: "1",
+      guild: { id: "1", name: "Wakanda", icon: null, owner: true },
+      category: "1",
+      date: "22/06 as 20:40",
+      description: "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+    {
+      id: "2",
+      guild: { id: "1", name: "Wakanda", icon: null, owner: true },
+      category: "1",
+      date: "22/06 as 20:40",
+      description: "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+  ];
+
+  const handleCategorySelect = useCallback(
+    (categoryId: string) => {
+      categoryId === category ? setCategory("") : setCategory(categoryId);
+    },
+    [category]
+  );
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Profile />
+        <ButtonAdd />
+      </View>
+      <CategorySelect categorySelected={category} setCategory={handleCategorySelect} />
+      <View style={styles.content}>
+        <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+        <FlatList
+          data={appointments}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  );
+}
