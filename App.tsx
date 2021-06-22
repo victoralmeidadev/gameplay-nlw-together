39cab872098a6827205583b9a361814dbbcdd5a8
Victor Almeida <victooralmeida1@gmail.com>
2021-06-21 21:39:48 -0300

    refactor: move status-bar to app file and add new theme props

--- App.tsx
+++ App.tsx
@@ -1,6 +1,29 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
+
 import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
-  return <SignIn />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    <AppLoading />;
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <SignIn />
+    </>
+  );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import { ButtonIcon } from "../../components/ButtonIcon";
 import IllustrationImage from "../../assets/illustration.png";
@@ -8,16 +8,7 @@ import { styles } from "./styles";
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <Image
-        source={IllustrationImage}
-        style={styles.image}
-        resizeMode="stretch"
-      />
+      <Image source={IllustrationImage} style={styles.image} resizeMode="stretch" />
       <View style={styles.content}>
         <Text style={styles.title}>
           Conecte-se {`\n`} e organize suas {`\n`} jogatinas
