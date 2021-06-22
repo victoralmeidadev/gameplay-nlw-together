2303cd68412fa21138818fc0c834c8eac3c0469d
Victor Almeida <victooralmeida1@gmail.com>
2021-06-22 00:25:59 -0300

    feat: add routes and svg config

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import AppLoading from "expo-app-loading";
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
 
-import { SignIn } from "./src/screens/SignIn";
+import { Routes } from "./src/routes";
 import { Background } from "./src/components/Background";
 
 export default function App() {
@@ -24,7 +24,7 @@ export default function App() {
   return (
     <Background>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <SignIn />
+      <Routes />
     </Background>
   );
 }
