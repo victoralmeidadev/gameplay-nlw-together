349a6d48d2451d8c35949d8578568118b966d610
Victor Almeida <victooralmeida1@gmail.com>
2021-06-23 22:28:53 -0300

    feat: create TextArea component

---
+++ src/components/TextArea/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { TextInput, TextInputProps } from "react-native";
+import { styles } from "./styles";
+
+export function TextArea({ ...props }: TextInputProps) {
+  return <TextInput style={styles.container} {...props} />;
+}
