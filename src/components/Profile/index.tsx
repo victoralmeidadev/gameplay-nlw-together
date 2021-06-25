8a98343560c95e2591b41d463079d4143bafb1d6
Victor Almeida <victooralmeida1@gmail.com>
2021-06-25 01:19:58 -0300

    feat: show user informations on profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "../../hooks/auth";
 import { Avatar } from "../Avatar";
 
 import { styles } from "./styles";
 
 export function Profile() {
+  const getMessage = useCallback(() => {
+    const messages = ["Hoje é dia de vitória", "Bora finalizar o dia invicto?", "Ouse desafiar"];
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    return message;
+  }, []);
+  const { user } = useAuth();
   return (
     <View style={styles.container}>
-      <Avatar urlImage="https://github.com/victoralmeidadev.png" />
+      <Avatar urlImage={user.avatar} />
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá,</Text>
-          <Text style={styles.username}>Victor</Text>
+          <Text style={styles.username}>{user.firstName}</Text>
         </View>
-        <Text style={styles.message}>Hoje é dia de vitória</Text>
+        <Text style={styles.message}>{getMessage()}</Text>
       </View>
     </View>
   );
