99d460c4f433776f92add8e1ac543d604ec80ec9
Victor Almeida <victooralmeida1@gmail.com>
2021-06-25 01:19:18 -0300

    feat: add discord configs

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,7 @@
+const { REDIRECT_URI } = process.env;
+const { SCOPE } = process.env;
+const { RESPONSE_TYPE } = process.env;
+const { CLIENT_ID } = process.env;
+const { CDN_IMAGE } = process.env;
+
+export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
---
+++ src/configs/index.ts
@@ -0,0 +1,1 @@
+export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE } from "./discordAuth";
