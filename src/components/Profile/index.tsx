024a4c6bc96c73458e1021b3b0224b7ae4b21fea
Victor Almeida <victooralmeida1@gmail.com>
2021-06-25 20:26:37 -0300

    refactor: add useMemo to messages

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useMemo } from "react";
 import { View, Text } from "react-native";
 import { useAuth } from "../../hooks/auth";
 import { Avatar } from "../Avatar";
@@ -6,7 +6,7 @@ import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export function Profile() {
-  const getMessage = useCallback(() => {
+  const getMessage = useMemo(() => {
     const messages = ["Hoje é dia de vitória", "Bora finalizar o dia invicto?", "Ouse desafiar"];
     const message = messages[Math.floor(Math.random() * messages.length)];
     return message;
@@ -20,7 +20,7 @@ export function Profile() {
           <Text style={styles.greeting}>Olá,</Text>
           <Text style={styles.username}>{user.firstName}</Text>
         </View>
-        <Text style={styles.message}>{getMessage()}</Text>
+        <Text style={styles.message}>{getMessage}</Text>
       </View>
     </View>
   );
