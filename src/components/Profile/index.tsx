820c76804397267c571c09da0eb7d0805f879b98
Victor Almeida <victooralmeida1@gmail.com>
2021-06-26 00:10:37 -0300

    feat: add callback to show signout on profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,20 +1,27 @@
 import React, { useMemo } from "react";
 import { View, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 import { useAuth } from "../../hooks/auth";
 import { Avatar } from "../Avatar";
 
 import { styles } from "./styles";
 
-export function Profile() {
+type Props = {
+  handleShowSignOut: () => void;
+};
+export function Profile({ handleShowSignOut }: Props) {
   const getMessage = useMemo(() => {
     const messages = ["Hoje é dia de vitória", "Bora finalizar o dia invicto?", "Ouse desafiar"];
     const message = messages[Math.floor(Math.random() * messages.length)];
     return message;
   }, []);
   const { user } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Avatar urlImage={user.avatar} />
+      <RectButton onPress={handleShowSignOut}>
+        <Avatar urlImage={user.avatar} />
+      </RectButton>
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá,</Text>
