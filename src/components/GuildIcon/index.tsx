70db57fc38a8af3181dfad46df2710d47f9d07fc
Victor Almeida <victooralmeida1@gmail.com>
2021-06-26 00:08:24 -0300

    refactor: change GuildIcon to use dinamic info

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Image } from "react-native";
-import { styles } from "./styles";
+import { Image, View } from "react-native";
 
-export function GuildIcon() {
+import { styles } from "./styles";
+import DiscordSvg from "../../assets/discord.svg";
+
+import { CDN_IMAGE } from "../../configs";
+
+type Props = {
+  guildId: string;
+  iconId: string | null;
+};
+export function GuildIcon({ guildId, iconId }: Props) {
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
+
   return (
-    <Image
-      source={{
-        uri: "https://img.utdstc.com/icon/a4a/8a6/a4a8a6b50439dd0a020cc3b1b971df3af09ae118578014799abd16ac23be6649:200",
-      }}
-      style={styles.image}
-      resizeMode="cover"
-    />
+    <View style={styles.container}>
+      {iconId ? (
+        <Image
+          source={{
+            uri,
+          }}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      ) : (
+        <DiscordSvg width={40} height={40} />
+      )}
+    </View>
   );
 }
